7cf20b807f10a5283d38bfa06248eb96c6c8f45a
￼Abhijit <42096027+abr-98@users.noreply.github.com>
2018-10-11 09:58:00 +0530

    Add files via upload

--- image_to_text_2.py
+++ image_to_text_2.py
@@ -1,14 +1,43 @@
 from pytesseract import *
+import sys
 from PIL import Image, ImageEnhance, ImageFilter
 import cv2
 import os
+#from scipy import imread, imsave, imresize
 import numpy as np
+import webbrowser
 import matplotlib
+
+#checking whether image is coloured or grayscale
+def is_grey_scale(img_path):
+    im = Image.open(img_path).convert('RGB')
+    w,h = im.size
+    for i in range(w):
+        for j in range(h):
+            r,g,b = im.getpixel((i,j))
+            if r != g != b: return False
+    return True
+
 
 #reading the image
 str=raw_input("Enter the image ")
 img0=cv2.imread(str)
 
+#maximize if not deskewed
+print("do you want to deskew the image *recommended for tilted images")
+ch1=raw_input("y-yes,n-no?")
+if ch1== 'n':
+	img0 = cv2.resize(img0, None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
+
+#initiating PIL
+img00=Image.open(str)
+
+if not is_grey_scale(str):
+	img00.show()
+	print("The image contains cooured  images if you convert it you may not get full accuracy")
+	ch=raw_input("Do you want  to continue? y-yes n-no")
+	if ch=='n':
+		sys.exit()
 #Creating a copy of the original
 img=img0.copy()
 
@@ -54,6 +83,17 @@ im = im.convert('1')
 im.save('test000.png')
 
 #image to text
-text = pytesseract.image_to_string(Image.open('test000.png'))
+text = pytesseract.image_to_string(Image.open('test000.png'),lang='eng')
+if text== "":
+	print("this is a logo like")
+#if text is an image or logo it will print the input as image
+	img00.show()
 os.remove('test000.png')
 print(text)
+#saving file to a text file
+text_file = open("Output.txt", "w")
+text_file.write("%s" % text)
+text_file.close()
+
+#to launch file in an editor
+webbrowser.open("Output.txt")
